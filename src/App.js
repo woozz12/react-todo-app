90637594e0de5c0ec6799cf7d331181bdb77f6ea
Leejunho <wpsxmfwnsgh@gmail.com>
2023-06-13 16:14:54 +0900

    todo-list ing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoTemplate from './component/TodoTemplate';
 
 function App() {
   return (
-    <h1>안녕안녕~~~</h1>
+    <TodoTemplate />
   );
 }
 
